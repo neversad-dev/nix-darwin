2fab0bfac991c30c00e2a24125fade24847a7f72
neversad <neversad@null.computer>
2025-05-13 17:18:12 +0300

    fix sketchybar language icon

--- home/darwin/sketchybar/config/plugins/get_input_source.sh
+++ home/darwin/sketchybar/config/plugins/get_input_source.sh
@@ -4,7 +4,7 @@
 plist_data=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources)
 current_input_source=$(echo "$plist_data" | plutil -convert xml1 -o - - | grep -A1 'KeyboardLayout Name' | tail -n1 | cut -d '>' -f2 | cut -d '<' -f1)
 
-if [ "$current_input_source" = "ABC" ]; then
+if [ "$current_input_source" = "ABC" ] || [ "$current_input_source" = "U.S." ]; then
     sketchybar --set input_source icon="󱌯"
 else
     sketchybar --set input_source icon="🇺🇦"

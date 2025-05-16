10da39fbb5dad5575f68dba0ce4db2910d49eb69
neversad <neversad@null.computer>
2025-05-16 11:24:03 +0300

    update capslock remapping: tap - escape, hold - ctrl

--- modules/system.nix
+++ modules/system.nix
@@ -211,8 +211,8 @@
       enableKeyMapping = true; # enable key mapping so that we can use `option` as `control`
 
       # NOTE: do NOT support remap capslock to both control and escape at the same time
-      remapCapsLockToControl = false; # remap caps lock to control, useful for emac users
-      remapCapsLockToEscape = true; # remap caps lock to escape, useful for vim users
+      remapCapsLockToControl = true; # remap caps lock to control, useful for emac users
+      remapCapsLockToEscape = false; # remap caps lock to escape, udfsseful for vim users
 
       # swap left command and left alt
       # so it matches common keyboard layout: `ctrl | command | alt`
